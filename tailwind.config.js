dce13e32de38721841bd398ca56fa29962035870
matteyu <matt.r.ilagan@gmail.com>
2023-09-29 15:28:14 -0700

    duplicate dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: 'media',
+  darkMode: ['media'],
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
     extend: {
@@ -20,7 +20,16 @@ module.exports = {
       {
         light: {
           ...require('daisyui/src/theming/themes')['[data-theme=light]'],
-          primary: '#f0f0f0'
+          'primary': "#0D2B8E",
+          'secondary': "#12AAE8",
+          'accent': "#B832BE",
+          'neutral': "#f0f0f0",
+          'base-100': "#0F014A",
+          'base-200': '#252d37',
+          'info': "#3055e8",
+          'success': "#43d6a0",
+          'warning': "#bb720c",
+          'error': "#e24028",
         },
       },
       {
