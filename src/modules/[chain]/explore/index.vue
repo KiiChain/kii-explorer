35324b286ce662076bfe3347df07762a6c4613f5
jowelyn <hornillajowelyn@gmail.com>
2023-10-31 21:17:14 +0800

    create explore route and menu item

---
+++ src/modules/[chain]/explore/index.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+// @ts-ignore
+import { useWalletStore } from '@/stores';
+
+const walletStore = useWalletStore();
+</script>
+<template>
+    <div>
+       <div class="font-bold text-2xl">Welcome {{ walletStore.shortAddress }}</div>
+    </div>
+</template>
+<route>
+    {
+      meta: {
+        i18n: 'explore',
+        order: 1
+      }
+    }
+</route> 
